3550693fa10352d09223c672db36e1cd39f7072e
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-13 15:35:25 +0300

    Add GetWrongHashFiles
    return file's pathes with wrong hash in IEnumberable<string>

--- FileHash/Models/FileInfoList.cs
+++ FileHash/Models/FileInfoList.cs
@@ -7,5 +7,12 @@ namespace FileHash.Models
     {
         [JsonProperty("files")]
         public IEnumerable<FileInfo> Files { get; set; }
+
+        public static IEnumerable<string> GetWrongHashFiles(FileInfoList fileInfoList)
+        {
+            return fileInfoList.Files
+                .Where(_ => !_.IsEqualHash(Hash.GetFileHash(_.Path)))
+                .Select(_ => _.Path).ToList();
+        }
     }
 }
