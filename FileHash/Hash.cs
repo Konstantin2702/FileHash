1b9f5dd96e46984f3c3297f6b577b7fc17fb0340
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-13 15:31:02 +0300

    fix GetFileHash refactoring

--- FileHash/Hash.cs
+++ FileHash/Hash.cs
@@ -6,12 +6,9 @@ namespace FileHash
     {
         public static string GetFileHash(string path)
         {
-
-            using (FileStream fs = File.OpenRead(path))
-            using (MD5 md5 = MD5.Create())
-            {
-                return BitConverter.ToString(md5.ComputeHash(fs)).Replace("-", string.Empty);
-            }
+            using FileStream fs = File.OpenRead(path);
+            using MD5 md5 = MD5.Create();
+            return BitConverter.ToString(md5.ComputeHash(fs)).Replace("-", string.Empty);
         }
     }
 }
