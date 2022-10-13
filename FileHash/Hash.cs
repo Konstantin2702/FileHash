6b7e580b11589c80c63778db71729e20866607da
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-13 15:07:16 +0300

    Add Hash.cs with method GetFileHash()
    GetFileHash(string path) return MD5 hash of file

---
+++ FileHash/Hash.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace FileHash
+{
+    public static class Hash
+    {
+        public static string GetFileHash(string path)
+        {
+
+            using (FileStream fs = File.OpenRead(path))
+            using (MD5 md5 = MD5.Create())
+            {
+                return BitConverter.ToString(md5.ComputeHash(fs)).Replace("-", string.Empty);
+            }
+        }
+    }
+}
