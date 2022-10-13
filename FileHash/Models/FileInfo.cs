57da40ca0ae5090a60271e649b64732fd427932a
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-13 10:52:03 +0300

    Add FileInfo, FileInfoList, Data.json
    FileInfoList consists of List<FileInfo> for work with Data.json

---
+++ FileHash/Models/FileInfo.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace FileHash
+{
+    public class FileInfo
+    {
+        [JsonProperty("path")]
+        public string Path { get; set; }
+
+        [JsonProperty("hash")]
+        public string Hash { get; set; }
+    }
+}
---
+++ FileHash/Models/FileInfoList.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+
+namespace FileHash.Models
+{
+    public class FileInfoList
+    {
+        [JsonProperty("files")]
+        public IEnumerable<FileInfo> Files { get; set; }
+    }
+}
