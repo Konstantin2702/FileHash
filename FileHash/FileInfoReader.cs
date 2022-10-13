81670911199885e26876ba21d96d6fba8f59586a
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-13 10:52:44 +0300

    Add FileInfoReader
    Read text from JSON to FileInfoList

---
+++ FileHash/FileInfoReader.cs
@@ -0,0 +1,19 @@
+﻿using FileHash.Models;
+using Newtonsoft.Json;
+
+namespace FileHash
+{
+    public static class FileInfoReader
+    {
+        public static FileInfoList Read(string path)
+        {
+            FileInfoList files = new();
+            using (StreamReader file = File.OpenText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                files = serializer.Deserialize(file, typeof(FileInfoList)) as FileInfoList;
+            }
+            return files;
+        }
+    }
+}
