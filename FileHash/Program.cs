b349b1de66aeeb2b3a1496a20c7518a3223cdcc3
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-14 13:54:19 +0300

    Add main program

--- FileHash/Program.cs
+++ FileHash/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using FileHash;
+using FileHash.Models;
+const string PATH = "../../../Data/Data.json";
+
+void PrintWrongHash()
+{
+    try
+    {
+        if (File.Exists(PATH))
+        {
+            FileInfoList files = FileInfoReader.Read(PATH);
+            var filesWithWrongHash = FileInfoList.GetWrongHashFiles(files);
+            if (files.Files.Count() == 0)
+            {
+                Console.WriteLine("There are no files with wrong hashes");
+            }
+            else
+            {
+                Console.WriteLine("Files with wrong hashes:");
+                foreach (var path in filesWithWrongHash)
+                {
+                    Console.WriteLine(path);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine($"File doesn't exits at this path {PATH}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+PrintWrongHash();
