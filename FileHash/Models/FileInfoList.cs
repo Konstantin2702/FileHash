10cbea97ad028b16f7c670804995373c17d06a05
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-14 13:52:50 +0300

    fix GetWrongHashFiles
    added check for files with wrong pathes

--- FileHash/Models/FileInfoList.cs
+++ FileHash/Models/FileInfoList.cs
@@ -1,6 +1,4 @@
 ﻿using Newtonsoft.Json;
-
-
 namespace FileHash.Models
 {
     public class FileInfoList
@@ -10,9 +8,22 @@ namespace FileHash.Models
 
         public static IEnumerable<string> GetWrongHashFiles(FileInfoList fileInfoList)
         {
-            return fileInfoList.Files
-                .Where(_ => !_.IsEqualHash(Hash.GetFileHash(_.Path)))
-                .Select(_ => _.Path).ToList();
+            List<string> result = new();
+            foreach(var file in fileInfoList.Files)
+            {
+                if (File.Exists(file.Path))
+                {
+                    if (!file.IsEqualHash(Hash.GetFileHash(file.Path)))
+                    {
+                        result.Add(file.Path);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"File doesn't exits at this path {file.Path}");
+                }
+            }
+            return result;
         }
     }
 }
