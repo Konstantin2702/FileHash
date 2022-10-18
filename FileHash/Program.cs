2cbae433da8f5b8366ab193869127d455b1beebf
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-18 15:50:31 +0300

    fix Main
    added command line args
    
    added PrintWrongHash - print files with wrong hash
    added PrintWrongPaths - print files with not computed and checked hash

--- FileHash/Program.cs
+++ FileHash/Program.cs
@@ -1,5 +1,78 @@
-﻿using FileHash;
-using FileHash.Models;
-const string PATH = "../../../Data/Data.json";
+﻿namespace FileHash
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var path = args[0];
+            while (true)
+            {
+                if (File.Exists(path))
+                {
+                    var files = FileInfoReader.Read(path);
+                    if (files is not null)
+                    {
+                        var filesWithWrongHash = HashGetHandler.GetWrongHashFiles(files.Files);
 
-FileInfoList.PrintWrongHash(PATH);
+                        PrintWrongHash(filesWithWrongHash);
+                        if (HashGetHandler.WrongFilePaths.Any())
+                        {
+                            PrintWrongPaths(HashGetHandler.WrongFilePaths);
+                        }
+
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Check content of your {path} and run the app again");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\nFile doesn't exits at this path {path}");
+                    Console.ResetColor();
+                    Console.WriteLine("Write right path of file");
+                    path = Console.ReadLine();
+                }
+            }
+        }
+
+        private static void PrintWrongHash(IEnumerable<string> files)
+        {
+            if (!files.Any())
+            {
+                Console.WriteLine("\nThere are no files with wrong hashes.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nFiles with wrong hashes:");
+                Console.ResetColor();
+                foreach (var path in files)
+                {
+                    Console.WriteLine(path);
+                }
+            }
+        }
+
+        private static void PrintWrongPaths(IEnumerable<Tuple<string, HashGetHandler.Errors>> wrongPaths)
+        {
+            if (wrongPaths.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nErrors in files:");
+                Console.ResetColor();
+                foreach (var tuple in wrongPaths)
+                {
+                    Console.WriteLine(tuple.Item2 + "\t" + tuple.Item1);
+                }
+            }
+
+        }
+    }
+}
+
+
+
