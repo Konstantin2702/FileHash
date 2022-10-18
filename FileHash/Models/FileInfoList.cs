845f9be64c28d0a8e7c8470659cf06bc566d4da2
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-18 15:47:41 +0300

    fix    removed GetWronghashFiles

--- FileHash/Models/FileInfoList.cs
+++ FileHash/Models/FileInfoList.cs
@@ -2,58 +2,6 @@
 {
     public class FileInfoList
     {
-        public IEnumerable<FileInfo> Files { get; set; }
-
-        public static IEnumerable<string> GetWrongHashFiles(FileInfoList fileInfoList)
-        {
-            List<string> result = new();
-            foreach(var file in fileInfoList.Files)
-            {
-                if (File.Exists(file.Path))
-                {
-                    if (!file.IsEqualHash(Hash.ComputeFileHash(file.Path)))
-                    {
-                        result.Add(file.Path);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"File in Data.json with path {file.Path} doesn't exits");
-                }
-            }
-            return result;
-        }
-
-        public static void PrintWrongHash(string filePath)
-        {
-            try
-            {
-                if (File.Exists(filePath))
-                {
-                    FileInfoList files = FileInfoReader.Read(filePath);
-                    var filesWithWrongHash = FileInfoList.GetWrongHashFiles(files);
-                    if (filesWithWrongHash.Count() == 0)
-                    {
-                        Console.WriteLine("There are no files with wrong hashes");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Files with wrong hashes:");
-                        foreach (var path in filesWithWrongHash)
-                        {
-                            Console.WriteLine(path);
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine($"File doesn't exits at this path {filePath}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+        public IEnumerable<FileInformation> Files { get; set; }
     }
 }
