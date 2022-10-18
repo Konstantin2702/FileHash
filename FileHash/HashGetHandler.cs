edf5e4cc67c97bcc146d6814ae357c2b93c25aaf
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-18 15:45:41 +0300

    Add HashGetHandler
    GetWronghashFiles replaced in this class. Added tuple WrongFilePaths that contains path file and error, that happened (error in reading or error with wrong path)

---
+++ FileHash/HashGetHandler.cs
@@ -0,0 +1,39 @@
+﻿using FileHash.Models;
+namespace FileHash
+{
+    public static class HashGetHandler
+    {
+        public enum Errors
+        {
+            WrongPath,
+            WrongRead
+        }
+        public static List<Tuple<string, Errors>> WrongFilePaths { get; set; } = new();
+
+        public static IEnumerable<string> GetWrongHashFiles(IEnumerable<FileInformation> files)
+        {
+            foreach (var file in files)
+            {
+                if (File.Exists(file.Path))
+                {
+                    var computeHash = Hash.ComputeFileHash(file.Path);
+                    if (computeHash is not null)
+                    {
+                        if (!file.IsEqualHash(computeHash))
+                        {
+                            yield return file.Path;
+                        }
+                    }
+                    else
+                    {
+                        WrongFilePaths.Add(new Tuple<string, Errors>(file.Path, Errors.WrongRead));
+                    }
+                }
+                else
+                {
+                    WrongFilePaths.Add(new Tuple<string, Errors>(file.Path, Errors.WrongPath));
+                }
+            }
+        }
+    }
+}
