31050ae5acedbcbf7f222a52215a5e94491e1b62
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-18 15:39:24 +0300

    fix naming

---
+++ FileHash/Models/FileInformation.cs
@@ -0,0 +1,14 @@
+﻿namespace FileHash.Models
+{
+    public class FileInformation
+    {
+        public string Path { get; set; }
+   
+        public string Hash { get; set; }
+
+        public bool IsEqualHash(string computedHash)
+        {
+            return Hash.Equals(computedHash, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
