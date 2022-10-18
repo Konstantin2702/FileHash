50b84e3804a4ab1a793ed1ead99f98be40520be4
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-18 15:41:20 +0300

    Fix Read Add CheckCorrectRead
    Chanhed handle of errors with adding method CheckCorrectRead. This method return true if json file was parse right

--- FileHash/FileInfoReader.cs
+++ FileHash/FileInfoReader.cs
@@ -7,13 +7,23 @@ namespace FileHash
     {
         public static FileInfoList Read(string path)
         {
-            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            try
             {
-
-                FileInfoList files = JsonSerializer.Deserialize<FileInfoList>(fs);
-                return files;
+                using var fs = new FileStream(path, FileMode.OpenOrCreate);
+                var files = JsonSerializer.Deserialize<FileInfoList>(fs);
+                return CheckCorrectRead(files)?files:null;
             }
-           
+            catch
+            {
+                return null;
+            }
+        }
+        private static bool CheckCorrectRead(FileInfoList files)
+        {
+            return !(files is null || files.Files.Any(_ => _.Path is null || _.Hash is null));
         }
     }
+
+   
 }
+
