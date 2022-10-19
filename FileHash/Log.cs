12cceb2e48e2d51edc780e3a3a2d5c934840f443
klaptev15337@gmail.com <klaptev15337@gmail.com>
2022-10-19 18:04:30 +0300

    Add Log,cs
    Contains information about exceptions

---
+++ FileHash/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHash
+{
+    public class Log
+    {
+        public List<string> Exceptions { get; set; } = new();
+    }
+}
